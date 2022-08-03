53aa2d766055e52921834f41617fd601ee97e861
drodange <71694755+drodange@users.noreply.github.com>
2022-08-03 14:14:39 +0200

    Add files via upload

---
+++ mediadevice-broadcast/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const port = 3333;
+
+const http = require("http");
+const server = http.createServer(app);
+
+app.use(express.static(__dirname + "/public"));
+
+server.listen(port,() => console.log(`Server is running on port ${port}`));
+
